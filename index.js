19b9aa5e46c1ce7b7e79c2e09c75a6a076e1b2a8
HoSeokMoon <adele007fall@naver.com>
2020-09-10 17:04:28 +0900

    mongoose 사용하여 db connection

--- index.js
+++ index.js
@@ -5,21 +5,30 @@ var static = require('serve-static');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var expressSession = require('express-session');
+var expressErrorHandler = require('express-error-handler');
+
+const mongoose = require('mongoose');
+
+// Define Schemes
+const todoSchema = new mongoose.Schema({
+  todoid: { type: Number, required: true, unique: true },
+  content: { type: String, required: true },
+  completed: { type: String, default: false }
+},
+{
+  timestamps: true
+});
+
+// Create Model & Export
 
 var app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(static (path.join(__dirname, 'public')));
 
-app.use(cookieParser());
-app.use(expressSession({
-    secret: 'my key',
-    resave: true,
-    saveUninitialized:true
-}))
-
 app.use(bodyParser.urlencoded({extended:false})); 
 app.use(bodyParser.json()); 
-
+ 
+ 
 app.use('/',(req,res,next)=>{
 
     var paramId = req.body.id || req.query.id;
@@ -31,4 +40,5 @@ app.use('/',(req,res,next)=>{
     res.write(`<div><p> ${paramPassword} <p></div>`);
     res.end();
 })
+ 
 http.createServer(app).listen(3000)
